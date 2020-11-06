39d622ac2fedeb5780aeab93c7351724d1e1cf00
Jhovin Bonifacio Lozano <jhovin.bonifacio@tecsup.edu.pe>
2020-11-06 11:37:12 -0500

    Initial Commit

---
+++ laboratorio7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  laboratorio7
+//
+//  Created by mbtec22 on 11/6/20.
+//  Copyright © 2020 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
